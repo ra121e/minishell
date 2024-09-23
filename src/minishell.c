65f25295e45238f7502373f043bba5e41b7c52ab
Xuan Zhi Lok <xlok@c2r3s12.42singapore.sg>
2024-09-23 19:47:49 +0800

    fine-tuned minishell.c
    add lexer() into readline
    removed redundant history WIP lines

--- src/minishell.c
+++ src/minishell.c
@@ -6,44 +6,27 @@
 /*   By: xlok <xlok@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 19:45:28 by xlok              #+#    #+#             */
-/*   Updated: 2024/09/22 19:43:15 by xlok             ###   ########.fr       */
+/*   Updated: 2024/09/23 19:44:55 by xlok             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//int	main()
-//{
-//	char		*input;
-//	char		*prompt;
-//	prompt = ft_strjoin(ft_strjoin(getenv("USER"), "@"), ":$");//TODO:free
-//	while (1)
-//	{
-//		input = readline(prompt);
-//		if (!input)
-//			break ;
-//		else if (*input)
-//			add_history(input);
-//		free(input);
-//	}
-//=========================TODO: REMOVE========================================
-//readline handles the up/down arrows to scroll through the in-memory
-//history list so there's no need to access it using other functions like
-//history_list() / history_get() which are forbidden by proj
-////	prints history
-//	HIST_ENTRY	**history;
-//	int			i;
-//
-//	history = history_list();
-//	i = -1;
-//	while (history[++i])
-//		printf("%s\n", history[i]->line);
-//=========================TODO: REMOVE========================================
-//}
-
-//test lexer
 int	main()
 {
-	lexer("4+ 2 -100 + 100 =42 < cat |echo dd");
-	printf("pwd: %s\n", pwd());
+	char		*input;
+	char		*prompt;
+	prompt = ft_strjoin(ft_strjoin(getenv("USER"), "@"), ":$");//TODO:free
+	while (1)
+	{
+		input = readline(prompt);
+		if (!input)
+			break ;
+		else if (*input)
+		{
+			add_history(input);
+			lexer(input);
+		}
+		free(input);
+	}
 }
