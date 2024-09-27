9105dac63815f2cda270f9b319ddd2e957c1aee4
Xuan Zhi Lok <xlok@c2r3s12.42singapore.sg>
2024-09-27 19:19:06 +0800

    lexer: single quotes handling
    lexer.c: added single quotes handling
    minishell.h: added struct ms for variables

--- src/lexer.c
+++ src/lexer.c
@@ -6,23 +6,40 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 20:36:49 by xlok              #+#    #+#             */
-/*   Updated: 2024/09/26 09:23:01 by athonda          ###   ########.fr       */
+/*   Updated: 2024/09/27 19:57:29 by xlok             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	is_space(char c)
+void	single_quotes(t_ms *ms, char *str, t_token **head)
 {
-	if (c == ' ' || c == '\t' || c == '\n' || \
-		c == '\r' || c == '\v' || c == '\f')
-		return (1);
-	return (0);
+	ms->end++;
+	while (str[ms->end] && str[ms->end] != '\'')
+		ms->end++;
+	if (!str[ms->end])
+	{
+		ms->token = ft_substr(str, ms->start, ms->end - ms->start);
+		if (!ms->token)
+			perror("token malloc error");//malloc protection
+		tokenize(head, ms->token, TK_WORD);
+		printf("Word\n");
+		ms->start = ms->end;
+		return ;
+	}
+	ms->start++;
+	ms->token = ft_substr(str, ms->start, ms->end - ms->start);
+	if (!ms->token)
+		perror("token malloc error");//malloc protection
+	tokenize(head, ms->token, TK_SINGLE_QUOTES);
+	printf("Single Quotes\n");
+	ms->end++;
+	ms->start = ms->end;
 }
 
 bool	is_delimiter(char c)
 {
-	char	delimiter[] = " =+-*/%()[]{}|&'";//TODO:minishell doesn't need arithmetic
+	char	delimiter[] = " =+-*/%()[]{}|&";
 	int		i;
 
 	i = -1;
@@ -31,9 +48,6 @@ bool	is_delimiter(char c)
 		if (c == delimiter[i])
 			return (1);
 	}
-	if (c == '"')
-		return (1);
-//TODO:handle redirection char
 	return (0);
 }
 
@@ -48,58 +62,67 @@ bool	is_builtin(char *str)
 //TODO:operator/assignment etc.? what word to choose apart from builtin, word etc.
 t_token	*lexer(char *str)
 {
-	int		start;
-	int		end;
-	int		len;
-	char	*token;
+	t_ms	*ms;
 	t_token	*head;
 
+	ms = malloc(sizeof(t_ms));
+	if (!ms)
+		perror("ms malloc error");//malloc protection
 	head = NULL;
-	start = 0;
-	end = 0;
-	len = (int)(ft_strlen(str));
-	while (end <= len)
+	ms->start = 0;
+	ms->end = 0;
+	ms->len = (int)(ft_strlen(str));
+	while (ms->end <= ms->len)
 	{
-		if (!is_delimiter(str[end]) && end < len)
-			end++;
-		else if (start == end)
+//	Handle quotes
+//	Handle redirections & AND/OR/asterisk
+//	Handle others
+//	Handle EOF
+		if (str[ms->end] == '\'')
+		{
+			single_quotes(ms, str, &head);//add str and head to struct ms?
+			continue ;
+		}
+		else if (!is_delimiter(str[ms->end]) && ms->end < ms->len)
+			ms->end++;
+		else if (ms->start == ms->end)
 		{
 /*
 1) few spaces at the end of str: str[end] comes to \0 and handled as delimiter
   -> add \0 condition in if	statement
 2) get substr and apply tokenize() on the substr which is delimiter
 */
-			if (str[end] != ' ' && str[end] != '\0')
+			if (str[ms->end] != ' ' && str[ms->end] != '\0')
 			{
-				token = ft_substr(str, start, 1);
-				tokenize(&head, token, TK_RESERVED);
-				printf("Delimiter A: %c\n", str[end]);
+				ms->token = ft_substr(str, ms->start, 1);
+				tokenize(&head, ms->token, TK_RESERVED);
+				printf("Delimiter A\n");
 			}
-			start = ++end;
+			ms->start = ++ms->end;
 		}
 		else
 		{
-			token = ft_substr(str, start, end - start);
-			if (is_builtin(token))
+			ms->token = ft_substr(str, ms->start, ms->end - ms->start);
+			if (is_builtin(ms->token))
 			{
-				tokenize(&head, token, TK_BUILTIN);
-				printf("Builtin: %s\n", token);
+				tokenize(&head, ms->token, TK_BUILTIN);
+				printf("Builtin\n");
 			}
 			else
 			{
-				tokenize(&head, token, TK_WORD);
-				printf("Word: %s\n", token);
+				tokenize(&head, ms->token, TK_WORD);
+				printf("Word\n");
 			}
-			if (end < len && str[end] != ' ')
+			if (ms->end < ms->len && str[ms->end] != ' ')
 			{
 /*
 get substr and apply tokenize() on the substr which is delimiter
 */
-				token = ft_substr(str, end, 1);
-				tokenize(&head, token, TK_RESERVED);
-				printf("Delimiter B: %c\n", str[end]);
+				ms->token = ft_substr(str, ms->end, 1);
+				tokenize(&head, ms->token, TK_RESERVED);
+				printf("Delimiter B\n");
 			}
-			start = ++end;
+			ms->start = ++ms->end;
 		}
 	}
 	return (head);
