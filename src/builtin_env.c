3c31399fc207758fc59ed3206dcc16397798311c
Xuan Zhi Lok <xlok@c2r4s3.42singapore.sg>
2024-10-28 20:11:51 +0800

    env error msg for options or arguments

--- src/builtin_env.c
+++ src/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 17:34:39 by athonda           #+#    #+#             */
-/*   Updated: 2024/10/27 14:04:26 by xlok             ###   ########.fr       */
+/*   Updated: 2024/10/28 20:11:04 by xlok             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,11 @@ void	builtin_env(t_ms *ms)
 {
 	int	i;
 
+	if (ms->cmd[1])
+	{
+		dprintf(2, "Subject requirement: env with no options or arguments\n");
+		return ;
+	}
 	i = 0;
 	while (ms->envp[i])
 	{
--- src/builtin_pwd.c
+++ src/builtin_pwd.c
@@ -6,16 +6,12 @@
 /*   By: xlok <xlok@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 17:08:56 by xlok              #+#    #+#             */
-/*   Updated: 2024/10/27 14:46:14 by xlok             ###   ########.fr       */
+/*   Updated: 2024/10/28 20:00:37 by xlok             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//TODO:pwd in bash returns from PCB/fd
-//if dir is rm by another process while current process is in it
-//current code returns error while pwd in bash returns dir from PCB/fd
-//but current code is not in a running state like bash yet so...
 void	builtin_pwd(void)
 {
 	char	*cwd;
