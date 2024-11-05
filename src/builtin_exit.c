f73e72cad39302fc349f00a5ee32f8edfaecc02e
Xuan Zhi Lok <xlok@student.42singapore.sg>
2024-11-06 07:50:08 +0800

    fix exit leak and refactor code

--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 21:58:55 by athonda           #+#    #+#             */
-/*   Updated: 2024/11/03 21:52:23 by athonda          ###   ########.fr       */
+/*   Updated: 2024/11/06 07:44:55 by xlok             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,52 @@
  *		- exit number and one more arguments
  */
 
+static void	clean_before_exit(t_ms *ms, unsigned char c)
+{
+	free(ms->cmd_envp);
+	free_str_array(ms->cmd);
+	cleanup(ms);
+	cleanup_final(ms);
+	exit(c);
+}
+
+static void	process_in_parent(t_ms *ms, unsigned char c)
+{
+	ft_dprintf(2, "exit\n");
+	if (!ms->cmd[1])
+		clean_before_exit(ms, 0);
+	else if (ms->cmd[2])
+	{
+		ft_dprintf(2, "bash: exit: too many arguments\n");
+		ms->exit_status = 1;
+	}
+	else
+		clean_before_exit(ms, c);
+}
+
+static void	process_in_child(t_ms *ms, unsigned char c)
+{
+	if (ms->cmd[2])
+	{
+		ft_dprintf(2, "bash: exit: too many arguments\n");
+		exit(1);
+	}
+	else
+		exit(c);
+}
+
+static void	process_arg(t_ms *ms, int i)
+{
+	if (!ft_isdigit(ms->cmd[1][i]))
+	{
+		ft_dprintf(2, "bash: exit: %s: numeric argument required\n", ms->cmd[1]);
+		if (ms->in_pipe == 0)
+			clean_before_exit(ms, 2);
+		else
+			exit(2);
+	}
+}
+
 void	builtin_exit(t_ms *ms)
 {
 	unsigned char	c;
@@ -37,11 +83,7 @@ void	builtin_exit(t_ms *ms)
 		i++;
 	while (ms->cmd[1] && ms->cmd[1][i])
 	{
-		if (!ft_isdigit(ms->cmd[1][i]))
-		{
-			ft_dprintf(2, "bash: exit: %s: numeric argument required\n", ms->cmd[1]);
-			exit(2);
-		}
+		process_arg(ms, i);
 		i++;
 	}
 	if (ms->cmd[1])
@@ -49,26 +91,7 @@ void	builtin_exit(t_ms *ms)
 	else
 		c = 0;
 	if (ms->in_pipe == 0)
-	{
-		ft_dprintf(2, "exit\n");
-		if (!ms->cmd[1])
-			exit(0);
-		else if (ms->cmd[2])
-		{
-			ft_dprintf(2, "bash: exit: too many arguments\n");
-			ms->exit_status = 1;
-		}
-		else
-			exit(c);
-	}
+		process_in_parent(ms, c);
 	else
-	{
-		if (ms->cmd[2])
-		{
-			ft_dprintf(2, "bash: exit: too many arguments\n");
-			exit(1);
-		}
-		else
-			exit(c);
-	}
+		process_in_child(ms, c);
 }
